0517f2a3621c4a90462db7f1b63a6398bc7a27cf
satish <satishwillowood@gmail.com>
2024-10-07 16:53:17 +0530

    dokgr

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
+// require("dotenv").config();
 // const taskRoutes = require("./routes/tasksRoutes");
 // const { errorHandler } = require("./middlewares");
 const cronJobs = require("./jobs/cronJobs");
